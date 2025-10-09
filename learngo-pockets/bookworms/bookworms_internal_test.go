9e2728799ebc4fedec81aba1f6b49411fa03751f
Paul Soladoye <paulsoladoye@geotab.com>
2025-10-09 10:29:36 -0400

    section 3.1 complete

---
+++ learngo-pockets/bookworms/bookworms_internal_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	handmaidsTale = Book{
+		Author: "Margaret Atwood", Title: "The Handmaid's Tale",
+	}
+	oryxAndCrake = Book{
+		Author: "Margaret Atwood", Title: "Oryx and Crake",
+	}
+    theBellJar = Book{
+        Author: "Sylvia Plath", Title: "The Bell Jar",
+    }
+    janeEyre = Book{
+        Author: "Charlotte Brontë", Title: "Jane Eyre",
+    }
+)
+
+func TestLoadBookworms(t *testing.T) {
+	type testCase struct {
+		name     string
+		filepath string
+		want     []Bookworm
+		wantErr  bool
+	}
+
+	tests := []testCase{
+		{
+			name:     "File does not exist",
+			filepath: "testdata/none.json",
+			want:     nil,
+			wantErr:  true,
+		},
+		{
+			name:     "File exists",
+			filepath: "testdata/bookworms.json",
+			want: []Bookworm{
+				{Name: "Fadi", Books: []Book{handmaidsTale, theBellJar}},
+				{Name: "Peggy", Books: []Book{oryxAndCrake, handmaidsTale, janeEyre}},
+			},
+			wantErr: false,
+		},
+	}
+
+    for _, tc := range tests {
+        t.Run(tc.name, func(t * testing.T) {
+            // Act
+            got, err := loadBookworms(tc.filepath)
+
+            if tc.wantErr {
+				assert.Nil(t, got)
+                require.Error(t, err)
+				return
+            }
+			assert.ElementsMatch(t, tc.want, got)
+        })
+    }
+}
