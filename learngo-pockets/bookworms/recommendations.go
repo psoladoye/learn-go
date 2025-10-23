a0eb55d37487a421230acab2119ddab2f8aa54fd
Paul Soladoye <paulsoladoye@geotab.com>
2025-10-23 15:21:55 -0400

    revisit section on recommendations

---
+++ learngo-pockets/bookworms/recommendations.go
@@ -0,0 +1,12 @@
+package main
+
+func recommendOtherBooks(bookworms []Bookworm) []Bookworm {
+    for _, bw := range bookworms {
+        for _, otbw := range bookworms {
+            if otbw.Name != bw.Name {
+                
+            }
+        }
+    }
+    return nil
+}
